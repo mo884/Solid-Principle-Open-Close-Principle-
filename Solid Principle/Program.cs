1ac4dd65e776aa760ca08a580bdc651280843ab8
mohamedessam77740@gmail.com <mohamedessam77740@gmail.com>
2023-07-08 15:05:38 +0200

    Add project files.

---
+++ Solid Principle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Rectangle(5,6);
+            shape.Area();
+        }
+    }
+}
---
+++ Solid Principle/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principle
+{
+    public class Rectangle : Shape
+    {
+        private int width;
+        private int height;
+        public Rectangle(int width , int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        
+        public int MyProperty { get; set; }
+        public override void Area()
+        {
+            Console.WriteLine($"The Area = {width * height} ");
+        }
+    }
+}
